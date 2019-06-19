791deb4deb07a19ec69ba219b772e8235038f433
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-06-19 16:30:47 +0530

    day#3-update#3

---
+++ libs/conceptarchitect/shapes/circle.py
@@ -0,0 +1,5 @@
+import math
+
+def perimeter(radius):
+    return 2*math.pi*radius if radius>0 else None
+    
---
+++ libs/conceptarchitect/shapes/triangle.py
@@ -0,0 +1,2 @@
+def perimeter(s1,s2,s3):
+    return s1+s2+s3 if s1>0 and s2>0 and s3>0 and s1+s2>s3 and s2+s3>s1 and s1+s3>s2 else None
